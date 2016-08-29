1514eb1b71f38bc0902e6931f1fb8f9b875a76ef
Rodolfo Cacacho <RodolfoC@172.20.14.197>
2016-08-29 09:54:54 -0600

    Actualizado clase metodo, añadido el constructor

--- Hoja6/src/Metodos.java
+++ Hoja6/src/Metodos.java
@@ -1,3 +1,4 @@
+import java.util.Set;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +9,18 @@
  *
  * @author Olivet
  */
-public class Metodos {
-    Factory fac = new Factory();
-    Set<E> 
+public class Metodos<E> {
+    Factory fabrica = new Factory();
+    /* Conjuntos que se utilizaran */
+    private Set<E> conjuntoP;
+    private Set<E> conjuntoE;
+    private String text;
+    
+    /* Constructor */
+    public Tables(int value){
+	conjuntoP = fabrica.setTable(value);
+	conjuntoE = fabrica.setTable(value);
+	text = "";
+    }
+    
 }
