e68a8998ab1ab134dc0e0db5a7a9f321cf005e1e
oli15238 <oli15238@uvg.edu.gt>
2016-08-28 20:13:52 -0600

    progra defensiva

--- Hoja6/src/Main.java
+++ Hoja6/src/Main.java
@@ -15,12 +15,31 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Bienvenido al programa");
         System.out.println();
+        int x = 0;
         //Hacer loop para progra defensiva
+        while (x == 0){
         System.out.println("Elija el numero de implementacion SET a utilizar: ");
         System.out.println("1.HashSet");
         System.out.println("2.TreeSet");
         System.out.println("3.LinkedHashSet");
         Scanner keyboard = new Scanner(System.in);
-        int x = keyboard.nextInt();
+        try{
+			    x = Integer.parseInt(keyboard.nextLine());
+		    } catch (NumberFormatException e){
+			    System.out.println();
+			    System.out.println("Error: opcion invalida, intentelo de nuevo.");
+			    System.out.println();
+			    x = 0;
+		    }
+        if (x < 1 || x > 3)
+            x = 0;
+        }
+        if (x == 1)
+            //Factory de HASHSET
+        if (x == 2)
+            //Factory de TREESET
+        if (x == 3)
+            //Factory de LINKEDHASHSET
+        
     }
 }
